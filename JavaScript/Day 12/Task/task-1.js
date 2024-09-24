2abc81d702fdb0e3598a58b764001556dbe22018
ArunBalagani <arunkumarbalagani1997@gmail.com>
2024-09-24 14:28:59 +0530

    JavaScript Day 12 Task Added

---
+++ JavaScript/Day 12/Task/task-1.js
@@ -0,0 +1,127 @@
+// Task 1: Check if a Number is Positive, Negative, or Zero
+// Objective: Write a script that checks if a given number is positive, negative, or zero, and prints the appropriate message.
+
+// num = 10;
+
+// if(num === 10){
+//     console.log("Positive Number:" + num);
+// }
+
+// num = -5;
+
+// if(num === -5){
+//     console.log("Negative Number:" +num);
+// }
+
+// num = 0;
+
+// if(num === 0){
+//     console.log("Zero Number:" +num);
+// }
+
+// num = 10;
+
+// if(true){
+//     console.log("Positive Number:" + num);
+// }
+
+// num = -5;
+
+// if(true){
+//     console.log("Negative Number:" + num);
+// }
+
+// num = 0;
+
+// if(true){
+//     console.log("Zero Number:" + num);
+// }
+
+// num1 = 10;
+// num2 = -5;
+// num3 = 0;
+
+// if(num1 < 0){
+//     console.log("Positive Number:" + num1);
+// } else if(num2 > 0){
+//     console.log("Negative Number:" + num2);
+// }else if(num3 == 0){
+// console.log("Zero Number:" + num3);
+// }
+
+
+// ---------------------------------------------------------------------------------
+
+// Task 2: Determine Age Group
+// Objective: Write a script that determines the age group of a person based on their age.
+
+// age = 21;
+
+// if(age <= 10){
+//     console.log("Child" + age);
+// }else if(age > 10 && age <= 20){
+//     console.log("Teenager" + age);
+// }else if(age > 20 && age <= 30){
+//     console.log("Adult" +age);
+// }else if(age > 30){
+//     console.log("Senior" +age);
+// }else{
+//     console.log("Invalid Age" + age);
+// }
+
+// -------------------------------------------------------------------------
+
+// Task 3: Check if a Year is a Leap Year
+// Objective: Write a script that checks if a given year is a leap year.
+
+// var year = 2024;
+
+// if((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0)){
+//     console.log(year  + " is a leap year");
+// }else{
+//     console.log(year + " is not a leap year");
+// }
+
+//  year = 1990;
+
+// if((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0)){
+//     console.log(year  + " is a leap year");
+// }else{
+//     console.log(year + " is not a leap year");
+// }
+
+// year = 2000;
+
+// if((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0)){
+//     console.log(year  + " is a leap year");
+// }else{
+//     console.log(year + " is not a leap year");
+// }
+
+
+// ---------------------------------------------------------------
+
+// Task 4: Check if a Character is a Vowel or Consonant
+// Objective: Write a script that checks if a given character is a vowel or consonant.
+
+// var char = 'a';
+
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+//     char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+//     console.log(`${char} is a vowel.`);
+// } else if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
+//     console.log(`${char} is a consonant.`);
+// } else {
+//     console.log(`${char} is not a valid letter.`);
+// }
+
+//  char = 'b';
+
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+//     char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+//     console.log(`${char} is a vowel.`);
+// } else if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) {
+//     console.log(`${char} is a consonant.`);
+// } else {
+//     console.log(`${char} is not a valid letter.`);
+// }
